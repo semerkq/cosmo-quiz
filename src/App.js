86f8107ea08769706187a7b80c5644220423bb82
semerkq <tatyana.sikhova.0505@mail.ru>
2026-03-12 01:51:51 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import { View, SplitLayout, SplitCol, ScreenSpinner } from '@vkontakte/vkui';
+import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
+
+import { Home } from './panels';
+import { DEFAULT_VIEW_PANELS } from './routes';
+
+export const App = () => {
+  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
+  const [fetchedUser, setUser] = useState();
+  const [popout, setPopout] = useState(<ScreenSpinner />);
+
+  useEffect(() => {
+    async function fetchData() {
+      const user = await bridge.send('VKWebAppGetUserInfo');
+      setUser(user);
+      setPopout(null);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <SplitLayout>
+      <SplitCol>
+        <View activePanel={activePanel}>
+          <Home id="home" fetchedUser={fetchedUser} />
+        </View>
+      </SplitCol>
+      {popout}
+    </SplitLayout>
+  );
+};
